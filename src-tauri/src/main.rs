026b62dd5b9682fa57a038e9c78ac505f2befa0d
jetstream0 <49297268+jetstream0@users.noreply.github.com>
2023-11-11 05:24:18 +0000

    generic tauri setup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,8 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() {
-    println!("Hello, world!");
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
